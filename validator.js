6efe00590ee645000574aea2b96fc8a5b0c404c8
termiyanc <eduard.termiyanc@mail.ru>
2020-05-25 10:49:29 +0300

    Замена var на let и const.

--- validator.js
+++ validator.js
@@ -1,5 +1,5 @@
-Validator = {
-
+// Класс производит валидацию полей формы
+const Validator = {
     //  Определение типов проверяемых значений
     types: {
         symbolic: {
@@ -24,7 +24,7 @@ Validator = {
     tooltip: {
         //  Функция создает элемент подсказки
         new: function (message) {
-            var tooltip = document.createElement('div');
+            const tooltip = document.createElement('div');
             tooltip.className = 'validator-tooltip';
             tooltip.innerText = message;
             return tooltip;
@@ -47,7 +47,7 @@ Validator = {
 
     //  Функция проверяет значения поля
     validateField: function (field) {
-        var trimmedValue = field.value.trim();
+        const trimmedValue = field.value.trim();
         //  Работаю с полем, имеющим значение и определение типа значения
         if (trimmedValue.length && field.dataset.validatorValueType) {
             //  Проверяю значение поля, результат проверки записываю в свойство, стилизую поле и добавляю или убираю подсказку
@@ -66,26 +66,25 @@ Validator = {
             removeClass(field, 'validator-valid|validator-invalid');
         }
     }
-
 };
 
 //  Функция добавляет класс элементу
 function addClass(el, classname) {
-    if (!new RegExp(classname).test(el.className)) {
+    if (!(new RegExp(classname).test(el.className))) {
         el.className += classname;
     }
 }
 
 //  Функция удаляет класс элемента
 function removeClass(el, classname) {
-    var classnameRegExp = new RegExp('(?:^| )(' + classname + ')(?:$| )');
+    const classnameRegExp = new RegExp('(?:^| )(' + classname + ')(?:$| )');
     if (classnameRegExp.test(el.className)) {
         el.className = el.className.replace(classnameRegExp, '');
     }
 }
 
 window.onload = function () {
-    var i, j;
+    let i, j;
     //  Работаю со всеми формами
     for (i in document.forms) {
         //  Работаю со всеми полями формы
@@ -97,8 +96,9 @@ window.onload = function () {
         }
         //  Определяю поведение при отправке формы
         document.forms[i].onsubmit = function () {
+            let i;
             //  Если есть хотя бы одно неправильное поле, не отправляю форму
-            for (var i in this.elements) {
+            for (i in this.elements) {
                 if (this.elements[i].valid === false) {
                     return false;
                 }
